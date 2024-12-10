94707c919ac0a8fabc2b4cd83db51a4ecffd1306
Noemi <noemi.munt.b@gmail.com>
2024-12-10 17:06:49 +0000

    Asks user to guess a letter from randomly selected word.

---
+++ milestone_2.py
@@ -0,0 +1,18 @@
+#%%
+
+#import modules
+import random
+
+word_list = ["mango", "kiwi", "banana", "peach", "cherry"]
+
+#generate random word from list
+word = random.choice(word_list)
+#print(word)
+
+guess = input("Please enter a single letter")
+# %%
+
+if len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oopsie! That's not a valid input.")
